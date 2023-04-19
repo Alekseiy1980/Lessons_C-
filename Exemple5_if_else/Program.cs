142181dc4776ac796823675c36f59a1d6ae30103
Алексей Василенко <Alekseiy80@gmail.com>
2023-04-19 23:21:51 +0300

    Exemple5_if_else

---
+++ Exemple5_if_else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write ("Введите имя пользователя : ");
+string userName = Console.ReadLine();
+if(userName == "Маша")
+{
+    Console.WriteLine("Ура, это Маша!!!");
+}
+else
+{
+   Console.Write("Привет, ");
+   Console.WriteLine(userName);
+}
