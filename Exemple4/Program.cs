9b0c2d35b641240282a6f157c0036ef75ce1692a
Алексей Василенко <Alekseiy80@gmail.com>
2023-04-19 22:51:23 +0300

    1

--- Exemple4/Program.cs
+++ Exemple4/Program.cs
@@ -1,3 +1,14 @@
-﻿double numberA = 12;
+﻿using System;
+
+namespace Exemple4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          double numberA = 12;
 double numberB = 5;
 Console.WriteLine(numberA / numberB);
+        }
+    }
+}
--- Sum/Program.cs
+++ Sum/Program.cs
@@ -1,3 +1,3 @@
-﻿int numberA = 3;
+﻿int numberA = new Random().Next(1,10);
 int numberB = 4;
 Console.WriteLine(numberA + numberB);
