6b155a5c92defb5d8b18373d16be296da7967920
Алексей Василенко <Alekseiy80@gmail.com>
2023-04-20 18:19:13 +0300

    Giriy

---
+++ Exemple_6_Max/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 1;
+int b = 210;
+int c = 7;
+int d = 3;
+int e = 8;
+
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
